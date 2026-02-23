46a65586a2a3b565a1331e4a9c6e92ec6c775253
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-02-22 22:04:42 -0500

    Experimenting with box component and trigger component

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/DungeonEscape/UHT/TriggerComponent.generated.h
@@ -0,0 +1,62 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "TriggerComponent.h"
+
+#ifdef DUNGEONESCAPE_TriggerComponent_generated_h
+#error "TriggerComponent.generated.h already included, missing '#pragma once' in TriggerComponent.h"
+#endif
+#define DUNGEONESCAPE_TriggerComponent_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class UTriggerComponent ********************************************************
+DUNGEONESCAPE_API UClass* Z_Construct_UClass_UTriggerComponent_NoRegister();
+
+#define FID_game_dev_unreal_CppCourseProjects_UnrealLessons_3_Dungeon_Escape_DungeonEscape_Source_DungeonEscape_TriggerComponent_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUTriggerComponent(); \
+	friend struct Z_Construct_UClass_UTriggerComponent_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend DUNGEONESCAPE_API UClass* Z_Construct_UClass_UTriggerComponent_NoRegister(); \
+public: \
+	DECLARE_CLASS2(UTriggerComponent, UBoxComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/DungeonEscape"), Z_Construct_UClass_UTriggerComponent_NoRegister) \
+	DECLARE_SERIALIZER(UTriggerComponent)
+
+
+#define FID_game_dev_unreal_CppCourseProjects_UnrealLessons_3_Dungeon_Escape_DungeonEscape_Source_DungeonEscape_TriggerComponent_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UTriggerComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	UTriggerComponent(UTriggerComponent&&) = delete; \
+	UTriggerComponent(const UTriggerComponent&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UTriggerComponent); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UTriggerComponent); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UTriggerComponent) \
+	NO_API virtual ~UTriggerComponent();
+
+
+#define FID_game_dev_unreal_CppCourseProjects_UnrealLessons_3_Dungeon_Escape_DungeonEscape_Source_DungeonEscape_TriggerComponent_h_12_PROLOG
+#define FID_game_dev_unreal_CppCourseProjects_UnrealLessons_3_Dungeon_Escape_DungeonEscape_Source_DungeonEscape_TriggerComponent_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_game_dev_unreal_CppCourseProjects_UnrealLessons_3_Dungeon_Escape_DungeonEscape_Source_DungeonEscape_TriggerComponent_h_15_INCLASS_NO_PURE_DECLS \
+	FID_game_dev_unreal_CppCourseProjects_UnrealLessons_3_Dungeon_Escape_DungeonEscape_Source_DungeonEscape_TriggerComponent_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class UTriggerComponent;
+
+// ********** End Class UTriggerComponent **********************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_game_dev_unreal_CppCourseProjects_UnrealLessons_3_Dungeon_Escape_DungeonEscape_Source_DungeonEscape_TriggerComponent_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/DungeonEscape/TriggerComponent.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Display, TEXT("Play has begun"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
