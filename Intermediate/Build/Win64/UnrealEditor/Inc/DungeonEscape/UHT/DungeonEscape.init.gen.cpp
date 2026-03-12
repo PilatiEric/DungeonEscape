0b07893bbeb13cb47f903e7609cac1a28be810d0
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2026-03-11 22:24:59 -0400

    Create CollectableItem class and tags

--- Intermediate/Build/Win64/UnrealEditor/Inc/DungeonEscape/UHT/DungeonEscape.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/DungeonEscape/UHT/DungeonEscape.init.gen.cpp
@@ -29,13 +29,13 @@ void EmptyLinkFunctionForGeneratedCodeDungeonEscape_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0xCF9BF394,
-				0xC4588BAF,
+				0xC46B6762,
+				0x6E6A5D1D,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_DungeonEscape.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_DungeonEscape.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_DungeonEscape(Z_Construct_UPackage__Script_DungeonEscape, TEXT("/Script/DungeonEscape"), Z_Registration_Info_UPackage__Script_DungeonEscape, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xCF9BF394, 0xC4588BAF));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_DungeonEscape(Z_Construct_UPackage__Script_DungeonEscape, TEXT("/Script/DungeonEscape"), Z_Registration_Info_UPackage__Script_DungeonEscape, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xC46B6762, 0x6E6A5D1D));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
